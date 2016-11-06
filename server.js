8b45bed02823a394d8fbf2a217d568445baab07f
sbrocks <shaury29.baranwal@gmail.com>
2016-11-06 14:32:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -83,6 +85,17 @@ var htmlTemplate=`
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input){
+    //how do we create a hash
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',functio(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
+})
 
 app.get('/:articleName',function(req,res){
     //articleName=articleOne
