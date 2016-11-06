c7680e841117f3be1f1cf6d542d28aa7ded0ed3d
sbrocks <shaury29.baranwal@gmail.com>
2016-11-06 14:43:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,10 +86,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash(input){
+function hash(input,salt){
     //how do we create a hash
     var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',functio(req,res){
